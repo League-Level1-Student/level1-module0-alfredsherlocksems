ca96cbcf8bf6c8375a5ba316088f203003476d33
root <root@f5fe811094bc>
2020-10-12 20:26:52 +0000

    Houses con..

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -9,22 +9,36 @@ public class Houses {
 	public void run() {
 
 	Random ran = new Random();	
-	int height =	ran.nextInt(101);
+	int q = ran.nextInt(2);
+	int hight = ran.nextInt(3);
 	drawHouse(100);
+	
+	if (q == 0) {
 	for (int i = 0; i < 9; i++) {		
-drawHouse(height);
+		int height = ran.nextInt(251);
+		drawHouse(height);
+	}
+		
+	if (q == 1) {	
+		for (int i = 0; i < 9; i++) {	
+		String heght = "small";	
+if (hight == 1) {
+		heght = "medium";
 }
-	
-	
-	
-	
-	
+if (hight == 2) {
+	heght = "large";
+}
+drawHouse(heght);	
+	}
+	}
+	}
+		
 	
 	}
 	
 void drawHouse(int height) {
 	rob.setSpeed(30);
-	rob.penDown();
+	rob.penDown();		
 	rob.move(height);
 	rob.turn(90);
 	rob.move(25);
@@ -34,7 +48,28 @@ void drawHouse(int height) {
 	rob.move(20);
 	rob.turn(-90);
 }
-	
+void drawHouse(String heght) {
+	int size = 100;
+	if (heght.equals("small")) {
+		size = 60;
+	}
+	if (heght.equals("medium")) {
+		size = 120;
+	}
+	if (heght.equals("large")) {
+		size = 250;
+	}
+	rob.setSpeed(30);
+	rob.penDown();
+	rob.move(size);
+	rob.turn(90);
+	rob.move(25);
+	rob.turn(90);
+	rob.move(size);
+	rob.turn(-90);
+	rob.move(20);
+	rob.turn(-90);
+}	
 	
 	
 	
