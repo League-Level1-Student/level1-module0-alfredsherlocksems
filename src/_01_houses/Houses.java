1ae63582bd8b83b3abe160f115ac57cc3ebd46e1
root <root@6bcbfed91d80>
2020-10-19 20:27:42 +0000

    Houses improvement

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,77 +1,108 @@
 package _01_houses;
 
+import java.awt.Color;
 import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	Robot rob = new Robot(90, 400);
+
 	public void run() {
 
-	Random ran = new Random();	
-	int q = ran.nextInt(2);
-	int hight = ran.nextInt(3);
-	drawHouse(100);
-	
-	if (q == 0) {
-	for (int i = 0; i < 9; i++) {		
-		int height = ran.nextInt(251);
-		drawHouse(height);
-	}
-		
-	if (q == 1) {	
-		for (int i = 0; i < 9; i++) {	
-		String heght = "small";	
-if (hight == 1) {
-		heght = "medium";
-}
-if (hight == 2) {
-	heght = "large";
-}
-drawHouse(heght);	
-	}
-	}
-	}
-		
-	
-	}
-	
-void drawHouse(int height) {
-	rob.setSpeed(30);
-	rob.penDown();		
-	rob.move(height);
-	rob.turn(90);
-	rob.move(25);
-	rob.turn(90);
-	rob.move(height);
-	rob.turn(-90);
-	rob.move(20);
-	rob.turn(-90);
-}
-void drawHouse(String heght) {
-	int size = 100;
-	if (heght.equals("small")) {
-		size = 60;
-	}
-	if (heght.equals("medium")) {
-		size = 120;
-	}
-	if (heght.equals("large")) {
-		size = 250;
-	}
-	rob.setSpeed(30);
-	rob.penDown();
-	rob.move(size);
-	rob.turn(90);
-	rob.move(25);
-	rob.turn(90);
-	rob.move(size);
-	rob.turn(-90);
-	rob.move(20);
-	rob.turn(-90);
-}	
-	
-	
-	
-}
+		Random ran = new Random();
+		int q = ran.nextInt(2);
+		if (q == 0) {
+			for (int i = 0; i < 10; i++) {
+				int col = ran.nextInt(151);
+				int red = col;
+				int blue = col;
+				int green = col;
+				Color colour = new Color(red, blue, green);
+				int height = ran.nextInt(251);
+				drawHouse(height, colour);
+			}
+		}
+			if (q == 1) {
+				for (int i = 0; i < 10; i++) {
+					int col = ran.nextInt(150);
+					int red = col;
+					int blue = col;
+					int green = col;
+					Color colour = new Color(red, blue, green);
+					int hight = ran.nextInt(3);
+					String heght = "small";
+					if (hight == 1) {
+						heght = "medium";
+					}
+					if (hight == 2) {
+						heght = "large";
+					}
+					drawHouse(heght, colour);
+				}
+			
+		}
 
+	}
+
+	void drawHouse(int height, Color colour) {
+		rob.setPenColor(colour);
+		rob.setSpeed(30);
+		rob.penDown();
+		int tip = 90;
+		int monkey = 0;
+		int bob = 0;
+		if (height < 125) {
+		tip = 45;	
+		monkey = 25;
+		bob = 45;
+		}
+		rob.move(height);
+		rob.turn(tip);
+		rob.move(25);
+		rob.turn(90);
+		rob.move(monkey);
+		rob.turn(bob);
+		rob.move(height);
+		rob.turn(-90);
+		rob.setPenColor(0, 150, 0);
+		rob.move(20);
+		rob.turn(-90);
+	}
+
+	void drawHouse(String heght, Color colour) {
+		int size = 100;
+		rob.setPenColor(colour);
+		if (heght.equals("small")) {
+			size = 60;
+		}
+		if (heght.equals("medium")) {
+			size = 120;
+		}
+		if (heght.equals("large")) {
+			size = 250;
+		}
+		rob.setSpeed(30);
+		rob.penDown();
+		int tip = 90;
+		int monkey = 0;
+		int bob = 0;
+		if (size < 125) {
+		tip = 45;	
+		monkey = 25;
+		bob = 45;
+		}
+		rob.move(size);
+		rob.turn(tip);
+		rob.move(25);
+		rob.turn(90);
+		rob.move(monkey);
+		rob.turn(bob);
+		rob.move(size);
+		rob.turn(-90);
+		rob.setPenColor(0, 150, 0);
+		rob.move(20);
+		rob.turn(-90);
+	}
+
+}
