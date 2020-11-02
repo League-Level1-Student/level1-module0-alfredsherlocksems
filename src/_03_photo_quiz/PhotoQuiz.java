5734471e1dc7e18b0f2fabb9ddd676fc664055f4
root <root@ad01f28becf4>
2020-11-02 21:24:24 +0000

    Photos.

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,33 +26,38 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-String url = "";
+String url = "https://www.hairybikers.com/uploads/images/_recipeImage/ChickenJalfrazi.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
-		// 3. use the "createImage()" method below to initialize your Component
-
+		//Done on line 3.
+// 3. use the "createImage()" method below to initialize your Component
+Component image = createImage(url);
 		// 4. add the image to the quiz window
-
+quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String curry = JOptionPane.showInputDialog("What type of indian food is this? (Example: If it's Chicken Korma, write 'Korma', with a capital letter.)");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if (curry.equals("Jalfrezi")) {
+	JOptionPane.showMessageDialog(null, "CORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	JOptionPane.showMessageDialog(null, "INCORRECT");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(image);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
-		// 11. add the second image to the quiz window
-
+String pic = "https://imageresizer.static9.net.au/Uufmp9I9BAnOZh6_WRdDl5KYBM8=/1200x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2Ff2fb5c32-3a5a-414e-aebc-03559838e758";
+		Component pic1 = createImage(pic);
+// 11. add the second image to the quiz window
+quizWindow.add(pic1);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String program = JOptionPane.showInputDialog("");
 		// 14+ check answer, say if correct or incorrect, etc.
 
 	}
